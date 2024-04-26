a4b16c8962d0951f8f3a61b7540bc462dd7202cc
Joakimanochie <anochietobe@gmail.com>
2024-04-26 15:31:58 +0100

    Datagenetor

--- src/Datagenerator.py
+++ src/Datagenerator.py
@@ -6,3 +6,18 @@ from src.utils import logging
 
 
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains import LLMChain  
+from langchain.chains import SequentialChain
+
+
+load_dotenv()
+
+key=os.getenv("Mykey")
+
+llm = ChatGoogleGenerativeAI(model="gemini-pro", google_api_key=key)
+
+
+template = """"
+
+
+"""
